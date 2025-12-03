55b751b170764c2afcce216dea7074583ed6b78b
타이가 <147749627+DevvTyga@users.noreply.github.com>
2025-12-03 12:43:59 +0900

    Update NineVersionLayout.tsx

--- layouts/NineVersionLayout.tsx
+++ layouts/NineVersionLayout.tsx
@@ -45,7 +45,7 @@ export default function NineVersionLayout() {
             ))}
             <div className="p-2">
               <p className="text-[6px] text-gray-900 font-light whitespace-nowrap">
-                @DevShunrises - 스키즈 취향표
+                @DevvTyga - 스키즈 취향표
               </p>
             </div>
           </div>
